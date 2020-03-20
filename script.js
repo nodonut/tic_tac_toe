3a942aaff657332864472f64d5a0dc7b00c9060d
Apoorv Sohal <apoorvsohal@protonmail.com>
2020-03-20 13:02:38 +0530

    added js

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ const playerFactory = (name, choice, moveset) => {
 };
 
 const displayController = player => {
+  const crossBox = document.createElement('span');
   gridBoxes.forEach(box => {
     box.addEventListener(
       'click',
       e => {
         if (player.choice == 'X') {
           e.target.classList += ' fa fa-times';
-        } else {
-          e.target.classList += ' fa fa-circle';
+          console.log(e.target.id);
+
+          const boxId = e.target.id;
+          player.moveset[boxId] = 'X';
         }
+        trackMoves(player);
       },
-      { once: true }
+      { once: false }
     );
   });
 };
@@ -23,15 +27,29 @@ const displayController = player => {
 const crossChoice = document.querySelector('.cross');
 const circleChoice = document.querySelector('.circle');
 
-crossChoice.addEventListener('click', () => {
-  const playerCross = playerFactory('cross', 'X', []);
-  displayController(playerCross);
-  console.log(playerCross);
-});
+crossChoice.addEventListener(
+  'click',
+  () => {
+    const playerCross = playerFactory('cross', 'X', {});
+    displayController(playerCross);
+    console.log(playerCross);
+  },
+  { once: true }
+);
 
-circleChoice.addEventListener('click', () => {
-  const playerCircle = playerFactory('circle', 'O', []);
-  displayController(playerCircle);
+circleChoice.addEventListener(
+  'click',
+  () => {
+    const playerCircle = playerFactory('circle', 'O', {});
+    displayController(playerCircle);
 
-  console.log(playerCircle);
-});
+    console.log(playerCircle);
+  },
+  { once: true }
+);
+
+function trackMoves(player) {
+  if (player.moveset.length >= 3) {
+    console.log(player.moveset);
+  }
+}
